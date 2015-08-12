d62f24435525521c808af71bce616625f338378b
Jason Lane & Jing Wait <student@epicodus.com>
2015-08-12 09:40:38 -0700

    Add make, model, year, and save methods. Also add clear and all class methods

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,9 +1,22 @@
 class Vehicle
   attr_reader :make, :model, :year
+  @@vehicle_array = []
   def initialize(make, model, year)
     @make = make
     @model = model
     @year = year
+  end
+
+  def save
+    @@vehicle_array.push(self)
+  end
+
+  def self.all
+    @@vehicle_array
+  end
+
+  def self.clear
+    @@vehicle_array = []
   end
 
 end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -3,6 +3,7 @@ require './lib/vehicle'
 
 describe Vehicle do
   before do
+    Vehicle.clear
     @test_vehicle = Vehicle.new('Toyota', 'Prius', 2000)
   end
   describe '#make' do
@@ -14,8 +15,28 @@ describe Vehicle do
   end
   describe '#model' do
     it 'returns the model of the vehicle' do
+      #test_vehicle.save
       expect(@test_vehicle.model).to eq "Prius"
 
+    end
+  end
+
+  describe '#save' do
+    it 'adds a vehicle to the array of saved vehicles' do
+      @test_vehicle.save
+      expect(Vehicle.all).to eq [@test_vehicle]
+    end
+  end
+
+  describe '.all' do
+    it 'is empty at first' do
+      expect(Vehicle.all).to eq []
+    end
+  end
+
+  describe '#year' do
+    it 'returns the year of the vehicle' do
+      expect(@test_vehicle.year).to eq 2000
     end
   end
 end
